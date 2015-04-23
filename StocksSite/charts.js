18c05fb8e24cedff833b436a322e9340e719d422
Zach Cotter <cotter.z@husky.neu.edu>
2015-04-22 23:25:44 -0400

    first chart with no data

--- StocksSite/charts.js
+++ StocksSite/charts.js
@@ -2,22 +2,22 @@ $(function () {
     //Obviously there will be more than one of these, and I will probably generate them dynamically using JS
     $('#graph1').highcharts({
         title: {
-            text: 'Stocks (Maybe a more Descriptive Title)',
+            text: 'Market Return vs Strategy Return',
             x: -20
         },
         subtitle: {
-            text: 'Maybe Something Else About Stock Things',
+            text: '',
             x: -20
         },
         xAxis: {
             title: {
-                text: 'Time'
+                text: 'Investments Made'
             }
             //looks like it will automagically figure out scale for us, but we can specify if needed
         },
         yAxis: {
             title: {
-                text: 'Price (USD)'
+                text: 'Total Return (USD)'
             }
         },
         tooltip: {
@@ -30,10 +30,10 @@ $(function () {
             borderWidth: 0
         },
         series: [{
-            name: 'Predicted',
+            name: 'Market Return',
             data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
         }, {
-            name: 'Actual',
+            name: 'Strategy Return',
             data: [-0.2, 0.8, 5.7, 11.3, 17.0, 22.0, 24.8, 24.1, 20.1, 14.1, 8.6, 2.5]
         }]
     });
